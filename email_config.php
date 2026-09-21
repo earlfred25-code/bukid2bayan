db5155f2eae33e66da758abe546243c0bc52e5b6
earlfred25-code <earlfred25@gmail.com>
2026-09-21 09:33:39 +0800

    Create email_config.php

---
+++ email_config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'host' => 'smtp.gmail.com',
+    'port' => 587,
+    'username' => 'bukid2bayan@gmail.com', // palitan mo ng gmail mo
+    'password' => 'abcd efgh ijkl mnop', // 16 chars App Password from Google
+    'from_email' => 'bukid2bayan@gmail.com',
+    'from_name' => 'Bukid2Bayan',
+];
+?>
