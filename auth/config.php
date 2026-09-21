587f6738eac88e2fed0f72ec9b3e0e5eb07acdc8
earlfred25-code <earlfred25@gmail.com>
2026-09-21 09:48:27 +0800

    Create auth configuration for social logins
    Add configuration for Google and Facebook authentication.

---
+++ auth/config.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'google' => [
+        'client_id' => 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com',
+        'client_secret' => 'YOUR_GOOGLE_CLIENT_SECRET',
+        'redirect_uri' => 'https://bukid2bayan.vercel.app/auth/google.php', // palitan mo ng domain mo
+    ],
+    'facebook' => [
+        'app_id' => 'YOUR_FACEBOOK_APP_ID',
+        'app_secret' => 'YOUR_FACEBOOK_APP_SECRET',
+        'redirect_uri' => 'https://bukid2bayan.vercel.app/auth/facebook.php',
+    ]
+];
